ba3174374900291c23be51cdac12de01ab4cf444
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-28 13:22:26 +0530

    get data from database

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.studentsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService{
     @Autowired
@@ -12,5 +14,10 @@ public class StudentServiceImpl implements StudentService{
     @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
     }
 }
