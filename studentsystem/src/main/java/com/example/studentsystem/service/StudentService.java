6cf056bf8b5297d77067eda42dbcbd7e3b12f47a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-28 13:15:08 +0530

    insert data into database

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.example.studentsystem.service;
 
+import com.example.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,16 @@
 package com.example.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.example.studentsystem.model.Student;
+import com.example.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
